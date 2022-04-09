0a55e010a87204e04d55bbaacd3cd1e86a0a59a1
Scovia57 <102589338+Scovia57@users.noreply.github.com>
2022-04-09 16:59:39 +0300

    Add files via upload

---
+++ sum of 5 numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a = 2;
+	int b = 2;
+	int c = 2;
+	int d = 2;
+	int e = 2;
+	int sum;
+	sum = a + b + c + d + e;
+	
+	printf("sum of 5 numbers%d", sum);
+	
+	return 0;
+}
